997dba47ecbb39d041501a06ea15b2fd21a6e6b2
FuSoraS <developer.FuSoraS@gmail.com>
2026-02-19 14:14:42 -0300

    refactor: clean up astro.config.mjs by removing site and base properties

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fusoras.github.io',
-  base: '/',
-
   vite: {
   },
 });
