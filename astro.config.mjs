1471937b9e384f041d24ecf75c2a9b696533f78f
FuSoraS <developer.FuSoraS@gmail.com>
2026-04-21 22:55:14 -0400

    feat: integrate lightningcss for enhanced CSS processing and add custom media queries

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        drafts: {
+          customMedia: true
+        }
+      }
+    },
   },
   i18n: {
     locales: ['en', 'es'],
